9a06c71148579b82ebdd5a5e0d9100fc72b46fc4
Pablo Ferreyra <pabloeferreyra@gmail.com>
2025-09-19 12:58:55 -0300

    invoice, imprime, y redirige a movimientos

---
+++ Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace CuentasCorrientes.Models
+{
+    public class Invoice
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Cuit { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public int Id { get; set; }
+    }
+}
